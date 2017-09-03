85bb25bb193f0c20233a7bc3a41cdc5b794a78ec
gproduct <gproduct1379@yahoo.com>
2017-09-03 15:41:38 +0200

    Added new icons for menu

--- app/src/main/java/rs/edu/etspupin/www/ets_official/dashboard.java
+++ app/src/main/java/rs/edu/etspupin/www/ets_official/dashboard.java
@@ -73,7 +73,7 @@ public class dashboard extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+/*
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
@@ -86,7 +86,7 @@ public class dashboard extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/rs/edu/etspupin/www/ets_official/main_class.java
+++ app/src/main/java/rs/edu/etspupin/www/ets_official/main_class.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/*TODO
+    1. The algorithm is sketched down needs to be imported here
+    2. Wait for the xml file
+    3. Start developing the algorithm which will include file download and xml parsing
+ */
 public class main_class extends AppCompatActivity {
 
     private Intent intent_setupclass, intent_dashboardclass;
